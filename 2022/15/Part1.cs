e5c6b86a04c532ffd24222f9a4f1f193ed587361
schlaubi77 <82316007+schlaubi77@users.noreply.github.com>
2022-12-24 12:14:35 +0100

    Day 15

---
+++ 2022/15/Part1.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+
+class Part1
+{
+    static void Main(string[] args)
+    {
+        string[] read = File.ReadAllLines("..\\input.txt", Encoding.UTF8);
+
+        int[][] sensors = new int[read.Length][]; // 3 entries: x, y, distance to next beacon
+
+        int highestX = 0;
+        int lowestX = int.MaxValue;
+        int highestDistance = 0;
+
+        for (int i = 0; i < read.Length; i++)
+        {
+            string[] numbers = read[i].Replace("Sensor at x=", "").Replace("y=", "").Replace(",", "").Replace(": closest beacon is at", "").Replace("x=", "").Split(" ");
+            int[] entry = new int[3];
+            entry[0] = int.Parse(numbers[0]);
+            entry[1] = int.Parse(numbers[1]);
+            entry[2] = Math.Abs(entry[0] - int.Parse(numbers[2])) + Math.Abs(entry[1] - int.Parse(numbers[3]));
+            sensors[i] = entry;
+            
+            highestX = Math.Max(highestX, entry[0]);
+            lowestX = Math.Min(lowestX, entry[0]);
+            highestDistance = Math.Max(highestDistance, entry[2]);
+        }
+
+        int cannotContain = -1; // I have to start at -1 for some reason
+
+        for (int i = lowestX - highestDistance; i <= highestX + highestDistance; i++)
+        {
+            foreach (int[] sensor in sensors)
+            {
+                if(Math.Abs(i - sensor[0]) + Math.Abs(2000000 - sensor[1]) <= sensor[2])
+                {
+                    cannotContain++;
+                    break;
+                }
+            }
+        }
+
+        Console.WriteLine(cannotContain);
+    }
+}
---
+++ 2022/15/Part2.cs
@@ -0,0 +1,102 @@
+﻿using System.Text;
+
+class Part2
+{
+    static void Main(string[] args)
+    {
+        string[] read = File.ReadAllLines("..\\input.txt", Encoding.UTF8);
+
+        int[][] sensors = new int[read.Length][]; // 3 entries: x, y, distance to next beacon
+
+        for (int i = 0; i < read.Length; i++)
+        {
+            string[] numbers = read[i].Replace("Sensor at x=", "").Replace("y=", "").Replace(",", "").Replace(": closest beacon is at", "").Replace("x=", "").Split(" ");
+            int[] entry = new int[3];
+            entry[0] = int.Parse(numbers[0]);
+            entry[1] = int.Parse(numbers[1]);
+            entry[2] = Math.Abs(entry[0] - int.Parse(numbers[2])) + Math.Abs(entry[1] - int.Parse(numbers[3]));
+            sensors[i] = entry;
+        }
+
+        foreach (int[] sensor in sensors)
+        {
+            for(int offsetX = -sensor[2]; offsetX <= sensor[2]; offsetX++)
+            {
+                if (sensor[0] + offsetX >= 4_000_001 || sensor[0] + offsetX < -1)
+                    continue;
+
+                int offsetY = sensor[2] - Math.Abs(offsetX);
+                if (sensor[1] + offsetY <= 4_000_001 && sensor[1] + offsetY > -1)
+                {
+                    // check outer side
+                    if (!isCovered(sensor[0] + offsetX + 1, sensor[1] + offsetY, sensors))
+                    {
+                        Console.WriteLine((sensor[0] + offsetX + 1) * 4_000_000L + (sensor[1] + offsetY));
+                        return;
+                    }
+                    if (offsetX > 0)
+                    {
+                        // check up
+                        
+                        if (!isCovered(sensor[0] + offsetX, sensor[1] + offsetY + 1, sensors))
+                        {
+                            Console.WriteLine((sensor[0] + offsetX) * 4_000_000L + (sensor[1] + offsetY + 1));
+                            return;
+                        }
+                    }
+                    else
+                    {
+                        // check down
+                        if (!isCovered(sensor[0] + offsetX, sensor[1] + offsetY - 1, sensors))
+                        {
+                            Console.WriteLine((sensor[0] + offsetX) * 4_000_000L + (sensor[1] + offsetY - 1));
+                            return;
+                        }
+                    }
+
+                }
+
+                if (sensor[1] - offsetY <= 4_000_001 && sensor[1] - offsetY > -1)
+                {
+                    // check outer side
+                    if (!isCovered(sensor[0] + offsetX - 1, sensor[1] - offsetY, sensors))
+                    {
+                        Console.WriteLine((sensor[0] - offsetX - 1) * 4_000_000L + (sensor[1] - offsetY));
+                        return;
+                    }
+
+                    if(offsetX > 0)
+                    {
+                        // check up
+                        if (!isCovered(sensor[0] + offsetX, sensor[1] - offsetY + 1, sensors))
+                        {
+                            Console.WriteLine((sensor[0] + offsetX) * 4_000_000L + (sensor[1] - offsetY + 1));
+                            return;
+                        }
+                    }
+                    else
+                    {
+                        // check down
+                        if (!isCovered(sensor[0] + offsetX, sensor[1] - offsetY - 1, sensors))
+                        {
+                            Console.WriteLine((sensor[0] + offsetX) * 4_000_000L + (sensor[1] - offsetY - 1));
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private static bool isCovered(int posX, int posY, int[][] sensors)
+    {
+        foreach (int[] sensor in sensors)
+        {
+            if(Math.Abs(posX - sensor[0]) + Math.Abs(posY - sensor[1]) <= sensor[2])
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
